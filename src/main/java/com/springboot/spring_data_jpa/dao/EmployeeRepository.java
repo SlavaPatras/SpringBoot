f358921a0b41762f0c676a15604d15b08d2b4c77
crm0234 <slava.patras@gmail.com>
2021-07-28 16:53:38 +0300

    Țfirst commit

---
+++ src/main/java/com/springboot/spring_data_jpa/dao/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.springboot.spring_data_jpa.dao;
+
+import com.springboot.spring_data_jpa.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}
